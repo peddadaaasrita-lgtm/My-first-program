cb0c9bfa3ef65bda986013b207e7fecc54e0f98c
PEDDADA AASRITA <peddadaaasrita@gmail.com>
2025-12-28 19:01:14 +0530

    Create usage of if else statement.java

---
+++ usage of if else statement.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=10;
+		if (a>5){
+		    System.out.println("The number is greater than 5");
+		}else{
+		    System.out.println("The number is less than 5");
+		}
+	}
+}
