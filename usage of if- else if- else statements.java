7c254d004d8ca6756eb59bacdee0439009e079d9
PEDDADA AASRITA <peddadaaasrita@gmail.com>
2025-12-28 19:08:35 +0530

    Create usage of if- else if- else statements.java

---
+++ usage of if- else if- else statements.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=0;
+		if(a<0){
+		    System.out.println("The number is a negative integer");
+		}else if(a>0){
+		    System.out.println("The number is a positive integer");
+		}else{
+		    System.out.println("The number is neither positive nor negative  integer");
+		}
+	}
+}
